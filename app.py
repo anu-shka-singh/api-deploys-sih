ff7646dec421dc9a2aa1a5f7e45b4f14d3c889b5
anu-shka-singh <anushkasingh2811@gmail.com>
2023-09-19 23:54:21 +0530

    update in routes

--- app.py
+++ app.py
@@ -53,6 +53,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return 'Welcome to the Text Summarizer API'
+
 # Initialize the summarizer
 model = Summarizer()
 
